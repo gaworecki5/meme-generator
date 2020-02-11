efe95e4c7e1beaf39cd1433ece9f0d02e7fc361f
Andy Gaworecki <gaworecki5@gmail.com>
2020-02-11 16:48:53 -0600

    still working on flask app

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-Meme-generator to create artists memes with quotes
+meme-generator
+    Creates memes of artwork with quoets about art/creativity.
 
-I am so upset right now about this project
+A README file is included in each module directory, and each README includes:
 
-why are there no changes to commit
+The class name.
+A brief describing the role-and-responsibilities of the module.
+A list of the module's dependencies.
+A few examples of how to use the module.
+Instructions for setting up and running the program are included in the project root README file.
--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import random
 import os
 import requests
 from flask import Flask, render_template, abort, request
-
-from QuoteEngine import *
+from QuoteEngine import Importer
 from MemeGenerator import *
 
 app = Flask(__name__)
@@ -46,3 +45,33 @@ def rand_meme():
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template("meme.html", path=path)
 
+
+# @app.route("/")
+# def home():
+#     return "Hello World!"
+
+
+# @app.route("/create", methods=["GET"])
+# def meme_form():
+#     """ User input for meme information """
+#     return render_template("meme_form.html")
+
+
+# @app.route("/create", methods=["POST"])
+# def meme_post():
+#     """ Create a user defined meme """
+
+#     # @TODO:
+#     # 1. Use requests to save the image from the image_url
+#     #    form param to a temp local file.
+#     # 2. Use the meme object to generate a meme using this temp
+#     #    file and the body and author form paramaters.
+#     # 3. Remove the temporary saved image.
+
+#     path = None
+
+#     return render_template("meme.html", path=path)
+
+
+if __name__ == "__main__":
+    app.run()
